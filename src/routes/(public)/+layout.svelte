15bd2e45b351888978db98226f8d90ce85dc2060
dilute111 <loshock666@gmail.com>
2026-07-14 13:55:34 +0500

    add: (public)/(private) folders, move: about to (public) group, blog to (private) group, refactor: routes/+layout.svelte to simple logic, let children check which routes is allowed

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import {getAuthStatus, onAuthChange} from "$lib/shared/auth";
+    import {onMount} from "svelte";
+
+    let isLoggedIn = $state(getAuthStatus())
+    let isHydrated = $state(false)
+
+    onMount(() => {
+        isLoggedIn = getAuthStatus()
+        isHydrated = true
+    })
+
+    onAuthChange(() => {
+        isLoggedIn = getAuthStatus()
+    })
+
+    let {children} = $props()
+</script>
+
+<main class="container">
+    {#if !isHydrated}
+        <div>Loading...</div>
+    {:else if !isLoggedIn}
+        <div class="auth-message">
+            <h2>Авторизируйтесь, чтобы открыть весь контент</h2>
+        </div>
+    {/if}
+</main>
+
+{@render children()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,6 @@
 
     import TheHeader from "$lib/components/TheHeader.svelte";
     import TheFooter from "$lib/components/TheFooter.svelte";
-    import {getAuthStatus, onAuthChange} from "$lib/shared/auth";
-    import {onMount} from "svelte";
-
-    let isLoggedIn = $state(getAuthStatus())
-    let isHydrated = $state(false)
-
-    onMount(() => {
-        isLoggedIn = getAuthStatus()
-        isHydrated = true
-    })
-
-    onAuthChange(() => {
-        isLoggedIn = getAuthStatus()
-    })
 
     let {children} = $props();
 </script>
@@ -23,15 +9,7 @@
 <TheHeader/>
 
 <main class="container">
-    {#if !isHydrated}
-        <div>Loading...</div>
-    {:else if isLoggedIn}
         {@render children() }
-    {:else}
-        <div class="auth-message">
-            <h2>Авторизируйтесь, чтобы продолжить</h2>
-        </div>
-    {/if}
 </main>
 
 <TheFooter/>
